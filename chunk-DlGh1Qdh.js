65b215a860fcfa541d48bade589f8b8a19020a00
hyper-weavers <info@hyperweavers.com>
2026-09-21 07:00:19 +0000

    Deploy palan App 🚀

---
+++ chunk-DlGh1Qdh.js
@@ -0,0 +1,3 @@
+import{Tt as ac,b as Fv,i as BI,zn as sc}from"./chunk-CVLk3ui4.js";var g=(()=>{class e{static ɵfac=function(t){return new(t||e)};static ɵcmp=Fv({type:e,selectors:[[`app-page-not-found`]],decls:3,vars:0,consts:[[1,`grid`,`h-full`,`place-content-center`,`bg-white`,`p-4`,`dark:bg-gray-900`],[1,`uppercase`,`tracking-widest`,`text-gray-500`,`dark:text-gray-400`]],template:function(t,d){t&1&&(sc(0,`div`,0)(1,`h1`,1),BI(2,` 404 | Not Found `),ac()())},styles:[`[_nghost-%COMP%]{flex:1}
+`]})}return e})();export{g as PageNotFoundPage};
+//# debugId=29a050a5-5a74-5654-8f49-f0dafd1b9c27
